22188925a775634faf7efe3de3d0e54043cbbc66
Mayank Aggarwal <70526600+Mayankagg44@users.noreply.github.com>
2022-09-06 18:03:24 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,23 @@
-def func(value):
-  return value
+import boto3
 
-print(func(18))
+#Describe RDS
+def status_rds():
+    client = boto3.client('rds')
+    response = client.describe_db_instances(
+        DBInstanceIdentifier='database-1'
+    )
+    print(response)
+
+    for i in response['DBInstances']:
+        if i['DBInstanceStatus'] == 'Available':
+            client.stop_db_instance(DBInstanceIdentifier = i['DBInstanceIdentifier'])
+            print('stopping DB instance {0}'.format(i['DBInstanceIdentifier']))
+        elif i['DBInstanceStatus'] == 'stopped':
+            client.start_db_instance(DBInstanceIdentifier = i['DBInstanceIdentifier'])
+            print('starting DB instance {0}'.format(i['DBInstanceIdentifier']))
+        elif i['DBInstanceStatus']=='starting':
+            print('DB Instance {0} is in starting state. Please stop the cluster after starting is complete'.format(i['DBInstanceIdentifier']))
+        elif i['DBInstanceStatus']=='stopping':
+            print('DB Instance {0} is already in stopping state.'.format(i['DBInstanceIdentifier']))
+
+status_rds()
