31904f8a07ecfe2b680d2ba3751d0304563d4c27
Mayank Aggarwal <70526600+Mayankagg44@users.noreply.github.com>
2022-09-07 16:21:41 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,37 +1,36 @@
 import boto3
 import sys
-import os
 
 status = sys.argv[1]
+list_db= sys.argv[2]
+client = boto3.client('rds')
 
 #Describe RDS
 def status_rds():
-    client = boto3.client('rds')
-    response = client.describe_db_instances(
-        DBInstanceIdentifier = os.environ['NAME']
-    )
-    print(response)
-
-    for i in response['DBInstances']:
-        if status.lower() =='start':
-            if i['DBInstanceStatus'] == 'available' or i['DBInstanceStatus'] == 'starting':
-                print("Already start")
-                sys.exit(1)
-            elif i['DBInstanceStatus'] == 'stopping':
-                print('The DB instance {0} is in stopping mode...Kindly wait for few mins'.format(i['DBInstanceIdentifier']))
-                sys.exit(1)
-            else:
-                client.start_db_instance(DBInstanceIdentifier = i['DBInstanceIdentifier'])
-                print('starting DB instance {0}'.format(i['DBInstanceIdentifier']))          
-        if status.lower() =='stop':
-            if i['DBInstanceStatus'] == 'stopped' or i['DBInstanceStatus'] == 'stopping':
-                print("Already stopped")
-                sys.exit(1)
-            elif i['DBInstanceStatus'] == 'starting':
-                print('The DB instance {0} is in starting mode...Kindly wait for few mins'.format(i['DBInstanceIdentifier']))
-                sys.exit(1)
-            else:
-                client.stop_db_instance(DBInstanceIdentifier = i['DBInstanceIdentifier'])
-                print('stopping DB instance {0}'.format(i['DBInstanceIdentifier']))
+    for db in list_db.split(","):
+        response = client.describe_db_instances(
+            DBInstanceIdentifier=db
+        )
+        for i in response['DBInstances']:
+            if status.lower() =='start':
+                if i['DBInstanceStatus'] == 'available' or i['DBInstanceStatus'] == 'starting':
+                    print("Already started")
+                    sys.exit(1)
+                elif i['DBInstanceStatus'] == 'stopping':
+                    print('The DB instance {0} is in stopping mode...Kindly wait for few mins'.format(i['DBInstanceIdentifier']))
+                    sys.exit(1)
+                else:
+                    client.start_db_instance(DBInstanceIdentifier = i['DBInstanceIdentifier'])
+                    print('starting DB instance {0}'.format(i['DBInstanceIdentifier']))          
+            if status.lower() =='stop':
+                if i['DBInstanceStatus'] == 'stopped' or i['DBInstanceStatus'] == 'stopping':
+                    print("Already stopped")
+                    sys.exit(1)
+                elif i['DBInstanceStatus'] == 'starting':
+                    print('The DB instance {0} is in starting mode...Kindly wait for few mins'.format(i['DBInstanceIdentifier']))
+                    sys.exit(1)
+                else:
+                    client.stop_db_instance(DBInstanceIdentifier = i['DBInstanceIdentifier'])
+                    print('stopping DB instance {0}'.format(i['DBInstanceIdentifier']))
 
 status_rds()
