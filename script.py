e68d14adec16689495e1fc8eb1a48c4fd9b43f6e
Mayank Aggarwal <70526600+Mayankagg44@users.noreply.github.com>
2022-09-07 11:29:20 +0530

    Update script.py

--- script.py
+++ script.py
@@ -17,6 +17,7 @@ def status_rds():
                 sys.exit(1)
             elif i['DBInstanceStatus'] == 'stopping':
                 print('The DB instance {0} is in stopping mode...Kindly wait for few mins'.format(i['DBInstanceIdentifier']))
+                sys.exit(1)
             else:
                 client.start_db_instance(DBInstanceIdentifier = i['DBInstanceIdentifier'])
                 print('starting DB instance {0}'.format(i['DBInstanceIdentifier']))          
@@ -26,6 +27,7 @@ def status_rds():
                 sys.exit(1)
             elif i['DBInstanceStatus'] == 'starting':
                 print('The DB instance {0} is in starting mode...Kindly wait for few mins'.format(i['DBInstanceIdentifier']))
+                sys.exit(1)
             else:
                 client.stop_db_instance(DBInstanceIdentifier = i['DBInstanceIdentifier'])
                 print('stopping DB instance {0}'.format(i['DBInstanceIdentifier']))
